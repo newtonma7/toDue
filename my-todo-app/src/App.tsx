6cb1581d02bd4e04630f6a6fc3e74102a46ca9e5
newtonma7 <newtonma7@gmail.com>
2025-10-07 23:20:27 -0400

    env setup

---
+++ my-todo-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <main className="py-10 h-screen">
+      <h1 className="font-bold text-3xl text-center">
+        Your Todos
+      </h1>
+    </main>
+  )
+}
+
+export default App
