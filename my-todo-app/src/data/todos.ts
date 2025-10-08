7bacb3c1563a68da3ae7d61561881b506c66c8c9
newtonma7 <newtonma7@gmail.com>
2025-10-08 00:05:03 -0400

    made todo type  and the todo list and the state change for checking a todo off

---
+++ my-todo-app/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { type Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "fart",
+        completed:false,
+    },
+    {
+        id: 2,
+        title: "fart2",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "fart3",
+        completed: false,
+    }
+];
